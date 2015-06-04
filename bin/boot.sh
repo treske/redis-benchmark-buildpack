8dadbc22f54f3dcc06917a7e321614898b625f3d
Thomas Reske <treske@pivotal.io>
2015-06-04 19:03:32 +0200

    Fix path for copy

--- bin/boot.sh
+++ bin/boot.sh
@@ -6,10 +6,12 @@ which ruby
 gem env
 
 echo "Setting path"
-export PATH=$PATH:$HOME/ruby/bin:$HOME/ruby_gems/bin
+export PATH=$HOME/ruby/bin:$HOME/ruby_gems/bin:$PATH
 export GEM_HOME=$HOME/ruby_gems
 export GEM_SPEC_CACHE=$HOME/ruby_gems_spec
 echo $PATH
+
+which ruby
 
 mkdir -p /tmp/staged/app/ruby/
 ln -s $HOME/ruby/bin /tmp/staged/app/ruby/bin
