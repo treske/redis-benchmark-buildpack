ae03d12da74c6587f162df193036f3839794f393
Thomas Reske <treske@pivotal.io>
2015-06-04 19:00:17 +0200

    Fix path for copy

--- bin/boot.sh
+++ bin/boot.sh
@@ -11,6 +11,9 @@ export GEM_HOME=$HOME/ruby_gems
 export GEM_SPEC_CACHE=$HOME/ruby_gems_spec
 echo $PATH
 
+mkdir -p /tmp/staged/app/ruby/
+ln -s $HOME/ruby/bin /tmp/staged/app/ruby/bin
+
 gem env
 
 echo "Starting app"
