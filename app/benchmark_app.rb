56d56ffe6fbd11d4e2f191d5d22d97d8124cdf38
Thomas Reske <treske@pivotal.io>
2015-06-05 11:20:02 +0200

    Clean up

--- app/benchmark_app.rb
+++ app/benchmark_app.rb
@@ -4,6 +4,7 @@ require 'open3'
 require 'json'
 require 'pp'
 require 'cf-app-utils'
+require 'yaml'
 
 enable :lock
 
@@ -11,14 +12,22 @@ $stdout.sync = true
 
 class RedisBenchmarkApp < Sinatra::Base
   get '/' do
+    # Get 
+    redis_benchmark_opts = YAML.load_file("redis_benchmark_spec.yml")x)
+    pp redis_benchmark_opts    
+
+    # Get Redis service credentials
     redis = CF::App::Credentials.find_by_service_name('redis')
-    
+    pp redis
+
+    # Concatenate Redis Benchmark command
     cmd = './cmd/redis-benchmark'
     cmd.concat(' -h ')
     cmd.concat(redis['hostname'])
     cmd.concat(' -p ')
     cmd.concat(redis['port'].to_s)
-    
+
+    # Stream output to clients
     stream do |outp|
       IO.popen(cmd, 'r') do |io|
         while line=io.gets
