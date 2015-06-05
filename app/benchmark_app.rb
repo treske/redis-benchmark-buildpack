4724a4867cfb8fa40cd39d81fc29f7976702d85e
Thomas Reske <treske@pivotal.io>
2015-06-05 10:17:34 +0200

    Fix parsing of VCAP_SERVICES

--- app/benchmark_app.rb
+++ app/benchmark_app.rb
@@ -2,6 +2,7 @@ require 'rubygems' if RUBY_VERSION < "1.9"
 require 'sinatra'
 require 'open3'
 require 'json'
+require 'pp'
 
 enable :lock
 
@@ -10,6 +11,8 @@ $stdout.sync = true
 class RedisBenchmarkApp < Sinatra::Base
   get '/' do
     services = JSON.parse(ENV['VCAP_SERVICES'])
+    pp services
+
     redis_key = services.keys.select { |svc| svc =~ /redis/i }.first
     redis = services[redis_key].first['credentials']
     
