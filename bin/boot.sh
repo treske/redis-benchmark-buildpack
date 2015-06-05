37af624ec947a420bfb1ada87c7dec34492a70c1
Thomas Reske <treske@pivotal.io>
2015-06-05 11:08:11 +0200

    Clean up

--- bin/boot.sh
+++ bin/boot.sh
@@ -4,5 +4,5 @@
 export GEM_HOME=$HOME/ruby_gems
 export GEM_SPEC_CACHE=$HOME/ruby_gems_spec
 
-# Start application using bundlder
+# Start application using bundler
 $HOME/ruby_gems/bin/bundle exec --verbose rackup config.ru -o 0.0.0.0 -p $PORT
